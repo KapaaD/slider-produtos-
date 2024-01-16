967b1a9310eb5f6fa697eddc8942b2a524daecf9
KappaD <121440624+KapaaD@users.noreply.github.com>
2024-01-16 18:53:43 +0000

    acaba pelo amor de Deus acaba...

--- script/like.js
+++ script/like.js
@@ -1,14 +1,14 @@
-document.addEventListener('click', event => {
+// document.addEventListener('click', event => {
 
-    if (event.target.classList.contains('slider')) {
+//     if (event.target.classList.contains('slider')) {
 
-        let iconeAntes = classList.contains("like")
-        let iconeDepois = classList.contains("like2")
+//         let iconeAntes = classList.contains("like")
+//         let iconeDepois = classList.contains("like2")
 
-        if (event.target.src.includes(iconeAntes)) {
-            event.target.src = iconeDepois
-        } else {
-            event.target.src = iconeAntes
-        }
-    }
-})
+//         if (event.target.src.includes(iconeAntes)) {
+//             event.target.src = iconeDepois
+//         } else {
+//             event.target.src = iconeAntes
+//         }
+//     }
+// })
--- script/produtos.js
+++ script/produtos.js
@@ -1,33 +1,54 @@
 $(document).ready(function () {
 
-  const codigo = `
-  <div class="card">
-            <div class="like"></div>
-            <a href="">
-                <img class="produto" src="https://static.vecteezy.com/ti/fotos-gratis/t2/22653879-fantasia-ilha-com-cachoeiras-3d-ilustracao-elementos-do-isto-imagem-mobiliado-de-nasa-generativo-ai-gratis-foto.jpg" alt="">
-            </a>
-            <h4 class="nome-produto">Nome do produto</h4>
-            <div class="rating">
-                &#9733;
-                &#9733;
-                &#9733;
-                &#9733;
-                &#9734;
-            </div>
-            <div class="price">
-                <h5>Preço</h5>
-                <h5>Preço promoção</h5>
-            </div>
-            <a class="button" href="carrinho.html">Adicionar ao carrinho</a>
-        </div>
-  `;
+  // const codigo = `
+  // <div class="card">
+  //           <div class="like"></div>
+  //           <a href="">
+  //               <img class="produto" src="https://static.vecteezy.com/ti/fotos-gratis/t2/22653879-fantasia-ilha-com-cachoeiras-3d-ilustracao-elementos-do-isto-imagem-mobiliado-de-nasa-generativo-ai-gratis-foto.jpg" alt="">
+  //           </a>
+  //           <h4 class="nome-produto">${element.name}</h4>
+  //           <div class="rating">
+  //               &#9733;
+  //               &#9733;
+  //               &#9733;
+  //               &#9733;
+  //               &#9734;
+  //           </div>
+  //           <div class="price">
+  //               <h5>Preço</h5>
+  //               <h5>Preço promoção</h5>
+  //           </div>
+  //           <a class="button" href="carrinho.html">Adicionar ao carrinho</a>
+  //       </div>
+  // `;
 
-  fetch('data/data.json')
+  fetch('data.json')
+    .then((Response) => Response.json())
+    .then((data) => {
+      data.forEach(element => {
+        console.log("===================================");
+        document.querySelector('.slider').insertAdjacentHTML('beforeend',  `
+        <div class="card">
+                  <div class="like"></div>
+                  <a href="">
+                      <img class="produto" src=${element.image} alt="${element.name}">
+                  </a>
+                  <h4 class="nome-produto" title="${element.name}">${element.name}</h4>
+                  <div class="rating">
+                  ${handleRating(element.rating)}
+                  </div>
+                  <div class="price">
+                      <h5>R$ ${handlePrice(element.price)}</h5>
+                      <h5>R$ ${handlePrice(element.price, true )}</h5>
+                  </div>
+                  <a class="button" href="carrinho.html">Adicionar ao carrinho</a>
+              </div>
+        `);
+        console.log("===================================");
+      });
+    })
+
     .then(() => {
-      for (i = 0; i < 10; i++) {
-        document.querySelector('.slider').insertAdjacentHTML('beforeend', codigo);
-      }
-    }).then(() => {
 
       $(".js-slider").slick({
         dots: true,
@@ -67,32 +88,26 @@ $(document).ready(function () {
 
 });
 
-// fetch('./data/data.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     data.forEach(element => {
-//       $(".js-slider").slick("slickAdd",
-//       //   `
-//       // <div class="card">
-//       //   <div class="like"></div>
-//       //   <a href="">
-//       //     <img class="produto" src="https://static.vecteezy.com/ti/fotos-gratis/t2/22653879-fantasia-ilha-com-cachoeiras-3d-ilustracao-elementos-do-isto-imagem-mobiliado-de-nasa-generativo-ai-gratis-foto.jpg" alt="">
-//       //   </a>
-//       //     <h4 class="nome-produto">Nome do produto</h4>
-//       //   <div class="rating">
-//       //     &#9733;
-//       //     &#9733;
-//       //     &#9733;
-//       //     &#9734;
-//       //     &#9734;  
-//       //   </div>
-//       //   <div class="price">
-//       //     <h5>Preço</h5>
-//       //     <h5>Preço promoção</h5>
-//       //   </div>
-//       //     <a class="button">Adicionar ao carrinho</a>
-//       // </div>
-//       // `
-//       );
-//     });
-//   })
+    function handleRating (rating) {
+      let htmlToReturn = "";
+      for (let i = 0; i < rating; i++) {
+        htmlToReturn = htmlToReturn + "&#9733;";
+      }
+
+      for (let i = 0; i < 5 - rating; i++) {
+        htmlToReturn = htmlToReturn + "&#9734;";
+      }
+
+      return htmlToReturn
+    }
+
+    function handlePrice(price, discount = false) {
+      if (discount) {
+        price = price * 0.9;
+      }
+
+      return price.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    }
